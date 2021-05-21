1873f8c1e4656bef78aa0fea7edbd49aa63f7927
Daniela Reijs <danielareijs@gmail.com>
2021-05-21 15:58:09 +0200

    Seeds files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  # get 'bookmarks/new'
-  # get 'bookmarks/create'
-  # get 'bookmarks/destroy'
-  # get 'lists/index'
-  # get 'lists/show'
-  # get 'lists/new'
-  # get 'lists/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists#index'
+ root to: 'lists#index'
   resources :lists, only: [:show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
