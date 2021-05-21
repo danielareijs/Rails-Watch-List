62409b4a5719a88a964eabc50746d20b4394d249
Daniela Reijs <danielareijs@gmail.com>
2021-05-21 15:32:02 +0200

    Installed cloudinary and implemented feature to add a photo to list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,21 +17,21 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to list_path(@list)
+      redirect_to root_path(@list)
     else
-      redirect_to lists_path, notice: @list.errors.full_messages.join
+      redirect_to root_path, notice: @list.errors.full_messages.join
     end
   end
 
   def destroy
     @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # db/seeds.rb
 require 'faker'
+require "open-uri"
+
+file = URI.open('https://source.unsplash.com/random')
+list = List.new(name: 'Favvosar')
+list.photo.attach(io: file, filename: 'file.png', content_type: 'image/png')
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
